f1adabb420ccf4b061105182358e587a473d315d
Benn Herrera <benn.herrera@pm.me>
2024-06-22 23:13:55 -0700

    some gen_project refinement.

--- gen_project.sh
+++ gen_project.sh
@@ -1,12 +1,50 @@
 #!/usr/bin/env bash
+THIS_SCRIPT=$(basename "${0}")
 THIS_DIR=$(dirname "${0}")
 # can't rely on realpath existing
 THIS_DIR=$(cd "${THIS_DIR}"; pwd)
 
+function is_true() {
+	case "${1}" in
+		# ${1^^} syntax for upper casing not available in old mac bash		
+		t*|T*|y*|Y*|1) return 0;;
+		"") return "${2:-1}";;
+	esac
+	return 1
+}
+
+function usage_and_die() {
+	cat << __EOF
+usage: ${THIS_SCRIPT} [--help] | [--clean] [--build] [cmake_arg1]...
+    note: bootstrap.h must have been run first.
+    --build: build and run tests
+    --clean: delete build directory first
+    any cmake args must come after --clean, --build, -G
+    some potential cmake args. see src/cmake/options.cmake:
+    	-DBNG_BUILD_TESTS=[TRUE|FALSE] 
+    	-DBNG_OPTIMIZED_BUILD_TYPE=[BNG_DEBUG|BNG_RELEASE]
+    	-DBNG_INCLUDE_BUILD_TESTS_IN_ALL=[TRUE|FALSE]
+__EOF
+	exit ${1:-0}
+}
+
+# respect envars
+GEN_CLEAN=${GEN_CLEAN:-""}
+BUILD=${BUILD:-""}
+
+while [[ -n "${1}" ]]; do
+	case "${1}" in
+		-h*|--h*|-u*|--u*) usage_and_die;;
+		--clean|-c) GEN_CLEAN=true; shift;;
+		--build|-b) BUILD=true; shift;;
+		*) break;;
+	esac
+done
+
 case "$(uname)" in
 	MINGW*) IS_WIN=true;;
 	Darwin*) IS_MAC=true;;
-	Linux*) IS_LNX=true;;
+	Linux*) IS_LNX=true; GENERATOR="-G=ninja";;
   *) echo "unsupported platform $(uname)" 1>&2; exit 1;;
 esac
 
@@ -17,27 +55,29 @@ if [[ ! -f .venv/.activate ]]; then
 	exit 1
 fi
 
-#source .venv/.activate
+if ${GEN_CLEAN:-false}; then
+	(/bin/rm -rf build 2>&1) > /dev/null
+fi
+
 mkdir -p build
 cd build
 
-cmake "${@}" ../src
-
-function is_true() {
-	case "$1" in
-		t*|T*|y*|Y*|1) return 0;;
-	esac
-	return 1
-}
+if ! cmake ${GENERATOR} "${@}" "../src"; then
+	exit 1
+fi
 
 if is_true ${BUILD:-false}; then
-	cmake --build . --target clean
-	cmake --build .
-	EC=0
-	for TEST in $(/bin/ls tests/*/test_* | grep -v 'pdb'); do
-		if ! "${TEST}"; then
-			EC=1
-		fi
-	done
-	exit ${EC}
+	cmake --build . --target clean && \
+	cmake --build . && \
+	(
+		export PATH=".:${PATH}"
+		cd tests/Debug
+		EC=0
+		for TEST in $(/bin/ls test_* | grep -v 'pdb'); do
+			if ! "${TEST}"; then
+				EC=1
+			fi
+		done
+		exit ${EC}
+	)
 fi
--- src/cmake/platform_config.cmake
+++ src/cmake/platform_config.cmake
@@ -36,12 +36,8 @@ else()
 	message(FATAL_ERROR "unsupported compiler.")
 endif()
 
+include("${CMAKE_INCLUDE}/options.cmake")
 
-set(BNG_BUILD_TESTS TRUE CACHE BOOL "add tests suites to project")
-set(BNG_INCLUDE_BUILD_TESTS_IN_ALL TRUE CACHE BOOL "rebuild tests when project source changes")
-
-set(BNG_OPTIMIZED_BUILD_TYPE BNG_DEBUG CACHE STRING "what it says on the tin")
-set_property(CACHE BNG_OPTIMIZED_BUILD_TYPE PROPERTY STRINGS BNG_DEBUG BNG_RELEASE)
 set_property(GLOBAL PROPERTY USE_FOLDERS ON)
 
 add_compile_definitions(
