d8030966e45d7a5b75a2320b575a1ed458bd14a1
Benn Herrera <bennherrera@pm.me>
2024-11-29 16:32:32 -0800

    flatbuffers dependency added.
    flatbuffers tool flatc set up.

--- src/cmake/third_party.cmake
+++ src/cmake/third_party.cmake
@@ -2,6 +2,10 @@ include_directories(
   "${VCPKG_INSTALLED_DIR}/${VCPKG_TARGET_TRIPLET}/include"
 )
 
+#if(NOT VCPKG_HOST_TRIPLET)
+#  message(FATAL_ERROR "WTF, VCPKG?")
+#endif()
+
 # centralize third party package finding here.
 # remember to src/vcpkg/vcpkg port add <dependency>
 
@@ -11,3 +15,17 @@ find_package(glm REQUIRED CONFIGURE)
 # for link libraries will need to add something like
 #   target_link_libraries(${TARGET} PRIVATE DEP_NAME::DEP_NAME)
 # to the CMakeLists.txt 
+find_package(flatbuffers REQUIRED CONFIGURE)
+
+
+# third party tools
+set(BNG_VCPKG_TOOLS_BIN_DIR "${VCPKG_INSTALLED_DIR}/${VCPKG_HOST_TRIPLET}/tools")
+
+set(Flatc_EXECUTABLE "${BNG_VCPKG_TOOLS_BIN_DIR}/flatbuffers/flatc${BNG_HOST_EXE_SUFFIX}")
+function(Flatc WORKING_DIR)
+  execute_process(COMMAND "${Flatc_EXECUTABLE}" ${ARGN}
+    WORKING_DIRECTORY "${WORKING_DIR}" COMMAND_ERROR_IS_FATAL ANY)
+endfunction()
+if(NOT EXISTS "${Flatc_EXECUTABLE}")
+  message(FATAL_ERROR "${Flatc_EXECUTABLE} does not exist.")
+endif()
